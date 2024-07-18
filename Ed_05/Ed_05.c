db5890702e809c1114ae3b7e6c7ca1e1ed1f588d
vitor <vitor.oliveira.rolla@gmail.com>
2024-07-18 10:18:14 -0300

    Resolução dos exercícios 11 ao 20

--- Ed_05/Ed_05.c
+++ Ed_05/Ed_05.c
@@ -13,6 +13,7 @@
 #include<string.h>  //Para trabalhar cadeia de caracteres
 #include<ctype.h>   //Para trabalhar aspectos das strings
 #include<stdbool.h> //Para trabalhar condicoes
+#include<math.h>
 #include "io.h"     //Uso opcional de leitura de dados
 
 //----------------------------------------------//
@@ -210,45 +211,251 @@ void metodo_10(){
     printf("%s%d\n", "Produto = ", multiplicar(5));
 }
 
+void Multiplos6(){
+
+    int n = ReadInt("Ler uma quantia");
+
+    int valor = 0;
+
+    for(int i = 0; i < n; i++){
+
+        printf("\n%d", (valor + 1) * 6);
+        valor++;
+    }
+
+    printf("\n");
+}
+
 void metodo_11(){
 
+    Multiplos6();
+}
+
+void Multiplos3e5(){
+
+    int n = ReadInt("Ler uma quantia");
+
+    int valor = 1;
+
+    for(int i = 0; i < n; i++){
+
+        if(valor % 3 == 0 && valor % 5 == 0){
+            printf("\n%d", valor);
+        }
+
+        valor = valor * 3;
+    }
+
+    printf("\n");
 }
 
 void metodo_12(){
 
+    Multiplos3e5();
+}
+
+void PotenciasDeQuatro(){
+
+    int n = ReadInt("Ler uma quantia");
+
+    int valor = 1;
+
+    for(int i = n; i > 0; i--){
+
+        valor = pow(4, i);
+
+        printf("\n%d", valor);
+
+        valor = valor / 4;
+    }
+    printf("\n");
 }
 
 void metodo_13(){
 
+    PotenciasDeQuatro();
+}
+
+void Multiplos6den(){
+
+    double num   = 1.0;
+    double den   = 6.0;
+    double valor = num / den;
+
+    int n = ReadInt("Ler uma quantia");
+
+    for(int i = 0; i < n; i++){
+
+        printf("\n%d: %.3lf / %.3lf = %.3lf", i, num, den, valor);
+        den = den + 6.0;
+    }
 }
 
 void metodo_14(){
 
+    Multiplos6den();
 }
+
+void potenciasDecrescentes(){
+
+    double n     = ReadDouble("Ler um numero");
+    double num   = 1.0;
+    double den   = n;
+    double valor = num / den;
+    int    x   = ReadInt("Ler uma quantia");
+
+
+    for(int i = n; i > 0; i--){
+
+        if(i % 2 == 0){
+            den = pow(n, i);
+            printf("\n%d: %.3lf / %.3lf = %.3lf", i, num, den, valor);
+        }
+    }  
+    printf("\n");   
+}
+
 
 void metodo_15(){
 
+    potenciasDecrescentes();
+}
+
+void SomaMod3eN5(){
+
+    int valor = 3;
+    int soma  = valor;
+    int n     = ReadInt("Defina a quantia de numeros");
+
+    for(int i = 0; i > n; i++){
+
+        if(valor % 5 == 0){
+            valor = valor + 3;
+        }
+
+        if(valor % 3 == 0){
+            valor = valor + 3;
+            soma = soma + valor
+
+            printf("\n%d: %.3lf / %.3lf = %.3lf", i, num, den, valor);
+        }
+    }
+    printf("\n");
 }
 
 void metodo_16(){
 
+    SomaMod3eN5();
+}
+
+void somaInvM6eN5(){
+
+    int n        = ReadInt("Defina a quantia de numeros"); 
+    double soma  = 0.0;
+    double num   = 1.0;
+    double dem   = 6.0;
+    double valor = 0.0;
+
+    for(int i = 0; i < n; i++){
+
+        valor = num / dem;
+
+        if((int)dem % 6 == 0){
+
+            if((int)dem % 5 == 0){
+
+               dem = dem + 6; 
+            }
+
+            soma = soma + valor;
+
+            printf("\n%d: %.3lf / %.3lf = %.3lf", i, num, den, valor);            
+        }
+    } 
 }
 
 void metodo_17(){
 
+    somaInvM6eN5();
+}
+
+void PArazaoVariavel(){
+
+    int soma  = 0;
+    int valor = 6;
+    int razao = 1;
+    int n     = ReadInt("Defina a quantia de numeros");
+
+    for(int i = 0; i < n; i++){
+
+        soma = soma + valor;
+        valor = valor + razao;
+        razao++;
+    }
+
+    printf("\nA soma eh %d", soma);
+    printf("\n");
 }
 
 void metodo_18(){
 
+    PArazaoVariavel();
+}
+
+void QuadradosNaturais(){
+
+    int n     = ReadInt("Defina a quantia de numeros");
+    int valor = 0;
+    int init  = 6;
+    int soma  = 0;
+
+    for(int i = 0; i < n; i++){
+
+        valor = pow(init, 2);
+        soma  = soma + valor;
+        init++;
+    }
+
+    printf("\nA soma eh %d", soma);
+    printf("\n");
 }
 
 void metodo_19(){
 
+    QuadradosNaturais();
 }
 
+void InversoQuadradosNaturais(){
+
+    int n        = ReadInt("Defina a quantia de numeros");
+    double valor = 0.0;
+    double num   = 1.0;
+    double den   = 6.0;
+    double razao = 0.0;
+    double soma  = 0.0;
+
+    for(int i = 0; i < n; i++){
+
+        den = den + razao;
+        razao++;
+    }
+
+    razao--;
+
+    for(int i = n; i > 0; i--){
+
+        valor = num / den;
+        soma  = soma + valor;
+        printf("\n%d: %.3lf / %.3lf = %.3lf", i, num, den, valor);
+        den   = den - razao;
+        razao--;
+    }
+
+}
 
 void metodo_20(){
 
+    InversoQuadradosNaturais();
 }
 
 void metodo_21(){
